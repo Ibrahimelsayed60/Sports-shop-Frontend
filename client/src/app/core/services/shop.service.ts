c5ab8002832629f274d84d7b60175aa4dc3be43e
Ibrahimelsayed60 <hemasayed600@gmail.com>
2025-04-26 19:31:59 +0300

    Add Search feature in shop

--- client/src/app/core/services/shop.service.ts
+++ client/src/app/core/services/shop.service.ts
@@ -5,6 +5,7 @@ import { Pagination } from '../../shared/models/pagination';
 import { Product } from '../../shared/models/product';
 import { Observable } from 'rxjs';
 import { ResultDto } from '../../shared/models/ResultDto';
+import { ShopParams } from '../../shared/models/shopParams';
 
 @Injectable({
   providedIn: 'root'
@@ -17,22 +18,33 @@ export class ShopService {
   types:string[] = [];
   brands:string[] = [];
 
-  getProducts(brands?:string[], types?:string[])
+  getProducts(shopParams: ShopParams)
   {
 
     let params = new HttpParams();
 
-    if(brands && brands.length > 0)
+    if(shopParams.brands.length > 0)
     {
-      params = params.append('brands',brands.join(','));
+      params = params.append('brands',shopParams.brands.join(','));
     }
 
-    if(types && types.length > 0)
+    if(shopParams.types.length > 0)
       {
-        params = params.append('brands',types.join(','));
+        params = params.append('types',shopParams.types.join(','));
       }
 
-    params = params.append('pageSize', 20);
+    if(shopParams.sort)
+    {
+      params = params.append('sort',shopParams.sort);
+    }
+
+    if(shopParams.search)
+    {
+      params = params.append('search',shopParams.search);
+    }
+
+    params = params.append('pageSize', shopParams.pageSize);
+    params = params.append('pageIndex', shopParams.pageNumber);
 
     return this.http.get<ResultDto<Pagination<Product>>>(this.baseUrl + 'products', {params});
   }
@@ -50,7 +62,7 @@ export class ShopService {
   getTypes()
   {
     if(this.types.length > 0) return;
-    return this.http.get<ResultDto<string[]>>(this.baseUrl+'products/types').subscribe({
+    return this.http.get<ResultDto<string[]>>(this.baseUrl + 'products/types').subscribe({
       next: response => this.types = response.data
     })
   }
