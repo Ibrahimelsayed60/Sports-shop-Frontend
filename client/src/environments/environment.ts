f5c1fa6a04748f5aa90c00819133b5dc60e922b0
Ibrahimelsayed60 <hemasayed600@gmail.com>
2025-05-02 14:19:05 +0300

    Add stripe service

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl:'api/'
+  apiUrl:'api/',
+  stripePublicKey: 'pk_test_51RKDgCE2DrnmP4rjQJfJW6Nrv8ekFLPAHLmeEQSwXTRWuJsS2zdEMAi31dx7YB5hJpn5ACVuokmZKQYfSoURsaQY00csRjTnKn'
 };
