f1b702c7b6b650b2ab2838e1f5fef711af5b80a0
Ibrahimelsayed60 <hemasayed600@gmail.com>
2025-05-04 15:52:01 +0300

    Update address for user

--- client/src/app/features/checkout/checkout.component.ts
+++ client/src/app/features/checkout/checkout.component.ts
@@ -6,7 +6,11 @@ import { MatButton } from '@angular/material/button';
 import { StripeService } from '../../core/services/stripe.service';
 import { StripeAddressElement } from '@stripe/stripe-js';
 import { SnackbarService } from '../../core/services/snackbar.service';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatCheckboxChange, MatCheckboxModule} from '@angular/material/checkbox';
+import { StepperSelectionEvent } from '@angular/cdk/stepper';
+import { Address } from '../../shared/models/user';
+import { firstValueFrom } from 'rxjs';
+import { AccountService } from '../../core/services/account.service';
 
 @Component({
   selector: 'app-checkout',
@@ -26,7 +30,9 @@ export class CheckoutComponent implements OnInit, OnDestroy{
 
   private stripeService = inject(StripeService);
   private snackbar = inject(SnackbarService);
+  private accountService = inject(AccountService);
   addressElement?: StripeAddressElement;
+  saveAddress = false;
 
   async ngOnInit() {
     try {
@@ -36,6 +42,39 @@ export class CheckoutComponent implements OnInit, OnDestroy{
     } catch (error:any) {
       this.snackbar.error(error.message);
     }
+  }
+
+  async onStepChange(event: StepperSelectionEvent){
+    if(event.selectedIndex === 1){
+      if(this.saveAddress){
+        const address = await this.getAddressFromStripeAddress();
+        address && firstValueFrom(this.accountService.updateAddress(address))
+      }
+    }
+  }
+
+
+  private async getAddressFromStripeAddress() : Promise<Address | null> {
+    const result = await this.addressElement?.getValue();
+    const address = result?.value.address;
+
+    if(address){
+      return{
+        line1: address.line1,
+        line2: address.line2 || undefined,
+        city: address.city,
+        country: address.country,
+        state: address.state,
+        postalCode: address.postal_code
+      }
+    }else{
+      return null;
+    }
+
+  }
+
+  onSaveAddressCheckBoxChange(event: MatCheckboxChange){
+    this.saveAddress = event.checked;
   }
 
   ngOnDestroy(): void {
