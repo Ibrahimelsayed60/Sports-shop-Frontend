d81ecd42462380bdcec184e78619136046a8d4f5
Ibrahimelsayed60 <hemasayed600@gmail.com>
2025-05-05 11:25:55 +0300

    Validate visa and create checkout success component

--- client/src/app/features/checkout/checkout-success/checkout-success.component.ts
+++ client/src/app/features/checkout/checkout-success/checkout-success.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-checkout-success',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButton,
+    RouterLink
+  ],
   templateUrl: './checkout-success.component.html',
   styleUrl: './checkout-success.component.scss'
 })
 export class CheckoutSuccessComponent {
+  today = new Date();
 
+  constructor(){
+    this.today = new Date();
+  }
 }
